cb91a5c6c68fdbea4699402330af036d5741c092
Masternode <masternode@gmail.com>
2025-10-20 22:36:30 -0600

    Update logs file, new format added

--- run-sample-logs.sh
+++ run-sample-logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while true; do
-   echo "A live from nginx container: $(date +'%H:%M:%S,%3N')"
-  sleep 5
+	echo "Alive from nginx-sample: v1.0, container: $(hostname), date: $(date +'%H:%M:%S,%3N')"
+  sleep 15
 done
